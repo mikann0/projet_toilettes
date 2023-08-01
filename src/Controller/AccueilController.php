5cf7cb524f870358a79ec174b07e87d118457ca3
mikann0 <118973525+mikann0@users.noreply.github.com>
2023-08-01 22:50:47 +0200

    feat: simplify call api

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -9,43 +9,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccueilController extends AbstractController
 {
 
-    private function callAPI($options){
-        extract($options);
-
-        $ch = curl_init();
-
-        $header = array(
-            "Content-Type: application/json"
-        );
-
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($ch, CURLOPT_HEADER, FALSE);
-
-        if($method)
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-
-        $response = curl_exec($ch);
-        $info = curl_getinfo($ch);
-        curl_close($ch);
-
-        return array('response' => $response, 'info' => $info);
-    }
-
-
     #[Route('/', name: 'app_accueil')]
     public function index(): Response
     {
-        $options = [
-            'url' => "https://data.grandlyon.com/fr/datapusher/ws/grandlyon/lyon.toilettepublique_latest/all.json?maxfeatures=-1&start=1",
-            'method' => "GET"
-        ];
-        $result = $this->callAPI($options);
+        $response = file_get_contents('https://data.grandlyon.com/fr/datapusher/ws/grandlyon/lyon.toilettepublique_latest/all.json?maxfeatures=-1&start=1');
 
-        $json = json_decode($result["response"]);
-        dd($json);
+        $response = json_decode($response);
+        // dd($response);
+
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
         ]);
