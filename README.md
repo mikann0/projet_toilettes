f17bcdd2f5bc9c169c16336606cb933d63511e52
mikann0 <118973525+mikann0@users.noreply.github.com>
2023-08-01 22:41:35 +0200

    feat: api call

--- README.md
+++ README.md
@@ -15,3 +15,11 @@ Pour utiliser ce projet en local, il faut faire:
 ```
 php -S localhost:8000 -t public
 ```
+
+## Attention !
+
+Quand on clone ce repository, il faut avoir configuré git pour ne pas forcer les sauts de ligne en CRLF:
+
+`git config --global core.autocrlf false`
+
+C'est important car les fichiers `*.sh` doivent contenir des `\n` (car exécutés sur Linux), pas des `\r\n` (de Windows).
--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -8,9 +8,44 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController
 {
+
+    private function callAPI($options){
+        extract($options);
+
+        $ch = curl_init();
+
+        $header = array(
+            "Content-Type: application/json"
+        );
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+        if($method)
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+
+        $response = curl_exec($ch);
+        $info = curl_getinfo($ch);
+        curl_close($ch);
+
+        return array('response' => $response, 'info' => $info);
+    }
+
+
     #[Route('/', name: 'app_accueil')]
     public function index(): Response
     {
+        $options = [
+            'url' => "https://data.grandlyon.com/fr/datapusher/ws/grandlyon/lyon.toilettepublique_latest/all.json?maxfeatures=-1&start=1",
+            'method' => "GET"
+        ];
+        $result = $this->callAPI($options);
+
+        $json = json_decode($result["response"]);
+        dd($json);
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
         ]);
