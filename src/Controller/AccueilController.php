8af80f59cfc9f6fc036681083f63b87d9646509f
mikann0 <118973525+mikann0@users.noreply.github.com>
2023-08-02 22:56:43 +0200

    feat: add filter

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -3,22 +3,38 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController
 {
 
-    #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public $toilettes;
+
+    // Constructor
+    public function __construct()
     {
         $response = file_get_contents('https://data.grandlyon.com/fr/datapusher/ws/grandlyon/lyon.toilettepublique_latest/all.json?maxfeatures=-1&start=1');
 
-        $response = json_decode($response);
-        // dd($response);
+        // Initialize properties with constructor parameters
+        $this->toilettes = json_decode($response, true)["values"];
+    }
 
+    #[Route('/', name: 'app_accueil')]
+    public function index(Request $request): Response
+    {
+        $searchValue = $request->query->get('searchValue');
+        if (!isset($searchValue) || $searchValue === '') {
+            $searchResult = $this->toilettes;
+        } else {
+            $searchResult = array_filter($this->toilettes, function ($item) use ($searchValue) {
+                return isset($item['codepost']) && $item['codepost'] == $searchValue;
+            });
+        }
         return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
+            'searchValue' => $searchValue,
+            'toilettes' => $searchResult,
         ]);
     }
 }
