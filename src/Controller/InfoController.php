6283ddfb0f851731dfe07ae41371f5bbda33fd39
mikann0 <118973525+mikann0@users.noreply.github.com>
2023-08-03 23:42:19 +0200

    feat: local api; new info controller

--- src/Controller/InfoController.php
+++ src/Controller/InfoController.php
@@ -3,31 +3,35 @@
 namespace App\Controller;
 
 use App\Repository\ToilettesRepository;
+use Psr\Log\LogLevel;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Log\Logger;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AccueilController extends AbstractController
+class InfoController extends AbstractController
 {
 
     public $toilettesRepository;
+    public $logger;
 
     // Constructor
     public function __construct(ToilettesRepository $repository)
     {
         // Initialize properties with constructor parameters
         $this->toilettesRepository = $repository;
+        $this->logger = new Logger();
     }
 
-    #[Route('/', name: 'app_accueil')]
-    public function index(Request $request): Response
-    {
-        $searchValue = $request->query->get('searchValue');
 
-        return $this->render('accueil/index.html.twig', [
-            'searchValue' => $searchValue,
-            'toilettes' => $this->toilettesRepository->search($searchValue),
+    #[Route('/{id}', name: 'app_info')]
+    public function index($id): Response
+    {
+        $this->logger->log(LogLevel::WARNING, "Request toilette with id=".$id);
+        $uneToilette = $this->toilettesRepository->uneToilette($id);
+        return $this->render('info/index.html.twig', [
+            'toilette' => $uneToilette,
         ]);
     }
 }
--- src/Repository/ToilettesRepository.php
+++ src/Repository/ToilettesRepository.php
@@ -2,32 +2,59 @@
 
 namespace App\Repository;
 
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Psr\Log\LogLevel;
+use Symfony\Component\HttpKernel\Log\Logger;
 
-class ToilettesRepository {
+class ToilettesRepository
+{
 
-    public $toilettes;
+    private $toilettes;
+    private $logger;
 
     // Constructor
     public function __construct()
     {
-        $response = file_get_contents('https://data.grandlyon.com/fr/datapusher/ws/grandlyon/lyon.toilettepublique_latest/all.json?maxfeatures=-1&start=1');
+        // $response = file_get_contents('https://data.grandlyon.com/fr/datapusher/ws/grandlyon/lyon.toilettepublique_latest/all.json?maxfeatures=-1&start=1');
+
+        $response = file_get_contents("build/files/all.json");
+
+        $this->logger = new Logger();
+        $this->logger->log(LogLevel::WARNING, "Create repository");
 
         // Initialize properties with constructor parameters
         $this->toilettes = json_decode($response, true)["values"];
     }
 
-    public function search($searchValue) {
+    public function search($searchValue)
+    {
+        $this->logger->log(LogLevel::INFO, "Searching value " . $searchValue);
         if (!isset($searchValue) || $searchValue === '') {
             $searchResult = $this->toilettes;
         } else {
             $searchResult = array_filter($this->toilettes, function ($item) use ($searchValue) {
                 $isCodePostal = isset($item['codepost']) && $item['codepost'] == $searchValue;
-                $isNom = isset($item['nom']) && strpos(strtolower($item['nom']),strtolower($searchValue)) !== false;
-                return $isCodePostal || $isNom;
+                $isNom = isset($item['nom']) && strpos(strtolower($item['nom']), strtolower($searchValue)) !== false;
+                $isAdresse = isset($item['adresse']) &&
+                    strpos(
+                        strtolower($item['adresse']),
+                        strtolower($searchValue)
+                    ) !== false;
+                return $isCodePostal || $isNom || $isAdresse;
             });
         }
         return $searchResult;
-
     }
 
+    public function uneToilette($id)
+    {
+
+        for ($i = 0; $i < count($this->toilettes); $i++) {
+            $item = $this->toilettes[$i];
+            if(isset($item['uid']) && $item['uid'] == $id) {
+                return $item;
+            }
+        }
+        return null;
+    }
 }
